9ad1b672af01a0a3b613b49077c35fb41cc14feb
water258 <weishi258@gmail.com>
2018-11-11 14:36:45 +0800

    a

--- .dotfiles/linux-env.sh
+++ .dotfiles/linux-env.sh
@@ -1,4 +1,5 @@
 export ZSH_THEME="powerlevel9k/powerlevel9k"
-export PATH=/usr/local/cuda-9.2/bin:$PATH
-export LD_LIBRARY_PATH=/usr/local/cuda-9.2/lib64:$LD_LIBRARY_PATH
 export GOPATH=$HOME/work/go-root
+export PATH=$GOPATH/bin:/usr/local/cuda-9.2/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda-9.2/lib64:$LD_LIBRARY_PATH
+
