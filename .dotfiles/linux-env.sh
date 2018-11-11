f1a45785e52db8df683c6b4acc697cb6ce6336a1
water258 <weishi258@gmail.com>
2018-11-11 14:10:11 +0800

    add gopath

--- .dotfiles/linux-env.sh
+++ .dotfiles/linux-env.sh
@@ -1,3 +1,4 @@
 export ZSH_THEME="powerlevel9k/powerlevel9k"
 export PATH=/usr/local/cuda-9.2/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/cuda-9.2/lib64:$LD_LIBRARY_PATH
+export GOPATH=$HOME/work/go-root
