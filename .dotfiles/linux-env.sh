a8da0e2d520f6459d6aa820977de63bc309df2c0
wei.shi <weishi258@gmail.com>
2019-04-14 22:26:51 -0400

    update linux env

--- .dotfiles/linux-env.sh
+++ .dotfiles/linux-env.sh
@@ -1,5 +1,5 @@
 export ZSH_THEME="powerlevel9k/powerlevel9k"
 export GOPATH=$HOME/Documents/work/go-root
-export PATH=$GOPATH/bin:/usr/local/cuda-9.2/bin:$PATH
-export LD_LIBRARY_PATH=/usr/local/cuda-9.2/lib64:$LD_LIBRARY_PATH
+export PATH=$GOPATH/bin:/usr/local/cuda/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 
