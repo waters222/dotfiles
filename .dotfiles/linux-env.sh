3acc2f242497c759317369960134b34fbab069f2
wei.shi <weishi258@gmail.com>
2019-04-26 13:00:35 -0400

    add go root, remove fucking rust

--- .dotfiles/linux-env.sh
+++ .dotfiles/linux-env.sh
@@ -1,5 +1,6 @@
 export ZSH_THEME="powerlevel9k/powerlevel9k"
 export GOPATH=$HOME/Documents/work/go-root
-export PATH=$GOPATH/bin:/usr/local/cuda/bin:$PATH
+export GOROOT=/usr/local/go
+export PATH=$GOROOT/bin:$GOPATH/bin:/usr/local/cuda/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 
